091e751776c0eec8a14eab5c46b182b5a2757736
Johnny Avakian <javak@yahoo.com>
2018-01-13 18:24:55 -0800

    wip2

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-$(function(){
+$(function() {
 
-  function getCssProps(){
-var left = getCssProperty("box", "left");
-var top = getCssProperty("box", "top");
-console.log(left+" "+top);
-  }
-$("#downb").click(function(){
-  $("#box").animate({
+    function getCssProps() {
+        var left = getCssProperty("box", "left");
+        var top = getCssProperty("box", "top");
+        console.log(left + " " + top);
+    }
+    $("#downb").click(function() {
+        $("#box").animate({
 
-      top:"+=40px",
-      fontSize:"+=2px"
-  },getCssProps)
-  })
-
-  $("#upb").click(function(){
-    $("#box").animate({
-        top:"-=40px",
-        fontSize:"-=2px"
-    },getCssProps)
+            top: "+=40px",
+            fontSize: "+=2px"
+        }, getCssProps)
     })
 
-    $("#leftb").click(function(){
-      $("#box").animate({
-          left:"-=40px",
-          opacity:"+=0.05"
-      },getCssProps)
-      })
-
-      $("#rightb").click(function(){
+    $("#upb").click(function() {
         $("#box").animate({
-            left:"+=40px",
-            opacity:"-=0.05"
-        },getCssProps)
-        })
+            top: "-=40px",
+            fontSize: "-=2px"
+        }, getCssProps)
+    })
 
-        function getCssProperty(elmId, property){
-   var elem = document.getElementById(elmId);
-   return window.getComputedStyle(elem,null).getPropertyValue(property);
-}
+    $("#leftb").click(function() {
+        $("#box").animate({
+            left: "-=40px",
+            opacity: "+=0.05"
+        }, getCssProps)
+    })
+
+    $("#rightb").click(function() {
+        $("#box").animate({
+            left: "+=40px",
+            opacity: "-=0.05"
+        }, getCssProps)
+    })
+
+    function getCssProperty(elmId, property) {
+        var elem = document.getElementById(elmId);
+        return window.getComputedStyle(elem, null).getPropertyValue(property);
+    }
 });
